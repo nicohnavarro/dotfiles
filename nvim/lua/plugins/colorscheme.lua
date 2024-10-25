11a64e2d90ba3b78f13167e9f92d30b4dfd28430
Nicolás Navarro <nicohnavarro@gmail.com>
2024-10-25 23:07:18 +0200

    Github configs

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -10,104 +10,6 @@ return {
       -- vim.cmd.colorscheme("lackluster-mint")
     end,
   },
-  -- {
-  --   "ellisonleao/gruvbox.nvim",
-  --   priority = 1000,
-  --   config = function()
-  --     require("gruvbox").setup({
-  --       terminal_colors = true, -- add neovim terminal colors
-  --       undercurl = true,
-  --       underline = true,
-  --       bold = false,
-  --       italic = {
-  --         strings = true,
-  --         emphasis = true,
-  --         comments = true,
-  --         operators = false,
-  --         folds = true,
-  --       },
-  --       strikethrough = true,
-  --       invert_selection = false,
-  --       invert_signs = false,
-  --       invert_tabline = false,
-  --       invert_intend_guides = false,
-  --       inverse = true, -- invert background for search, diffs, statuslines and errors
-  --       contrast = "", -- can be "hard", "soft" or empty string
-  --       palette_overrides = {
-  --         -- Grayscale palette
-  --         -- dark0_hard = "#121212",
-  --         -- dark0 = "#1e1e1e",
-  --         -- dark0_soft = "#242424",
-  --         -- dark1 = "#2c2c2c",
-  --         -- dark2 = "#373737",
-  --         -- dark3 = "#484848",
-  --         -- dark4 = "#5a5a5a",
-  --         -- light0_hard = "#f5f5f5",
-  --         -- light0 = "#e0e0e0",
-  --         -- light0_soft = "#d6d6d6",
-  --         -- light1 = "#cccccc",
-  --         -- light2 = "#bdbdbd",
-  --         -- light3 = "#9e9e9e",
-  --         -- light4 = "#757575",
-  --         --
-  --         -- bright_red = "#b31f19",
-  --         -- -- bright_green = "#b8bb26",
-  --         bright_green = "#ebdbb2",
-  --         bright_yellow = "#fabd2f",
-  --         -- bright_blue = "#fbf1c7",
-  --         -- bright_purple = "#b16286",
-  --         -- bright_aqua = "#689d6a",
-  --         -- bright_orange = "#d65d0e",
-  --         --
-  --         -- Neutral colors remain grayscale
-  --         -- neutral_red = "#8c8c8c",
-  --         -- neutral_green = "#898989",
-  --         -- neutral_yellow = "#949494",
-  --         -- neutral_blue = "#6f6f6f",
-  --         -- neutral_purple = "#787878",
-  --         -- neutral_aqua = "#676767",
-  --         -- neutral_orange = "#747474",
-  --         --
-  --         -- -- Faded colors remain grayscale
-  --         -- faded_red = "#5e5e5e",
-  --         -- faded_green = "#606060",
-  --         -- faded_yellow = "#646464",
-  --         -- faded_blue = "#525252",
-  --         -- faded_purple = "#555555",
-  --         -- faded_aqua = "#4f4f4f",
-  --         -- faded_orange = "#595959",
-  --         --
-  --         -- -- Dark and light variations remain grayscale
-  --         -- dark_red_hard = "#2c2c2c",
-  --         -- dark_red = "#333333",
-  --         -- dark_red_soft = "#3a3a3a",
-  --         -- light_red_hard = "#bcbcbc",
-  --         -- light_red = "#ababab",
-  --         -- light_red_soft = "#9e9e9e",
-  --         --
-  --         -- dark_green_hard = "#2a2a2a",
-  --         -- dark_green = "#303030",
-  --         -- dark_green_soft = "#383838",
-  --         -- light_green_hard = "#c0c0c0",
-  --         -- light_green = "#d3d3d3",
-  --         -- light_green_soft = "#e0e0e0",
-  --         --
-  --         -- dark_aqua_hard = "#2b2b2b",
-  --         -- dark_aqua = "#323232",
-  --         -- dark_aqua_soft = "#3a3a3a",
-  --         -- light_aqua_hard = "#c7c7c7",
-  --         -- light_aqua = "#d6d6d6",
-  --         -- light_aqua_soft = "#e5e5e5",
-  --         --
-  --         gray = "#8a8a8a",
-  --       },
-  --       overrides = {},
-  --       dim_inactive = false,
-  --       transparent_mode = true,
-  --     })
-  --     vim.cmd("colorscheme gruvbox")
-  --   end,
-  -- },
   {
     "projekt0n/github-nvim-theme",
     name = "github-theme",
@@ -121,17 +23,17 @@ return {
           compile_file_suffix = "_compiled", -- Compiled file suffix
           hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
           hide_nc_statusline = true, -- Override the underline style for non-active statuslines
-          transparent = false, -- Disable setting bg (make neovim's background transparent)
+          transparent = true, -- Disable setting bg (make neovim's background transparent)
           terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
           dim_inactive = false, -- Non focused panes set to alternative background
           module_default = true, -- Default enable value for modules
           styles = { -- Style to be applied to different syntax groups
             comments = "italic", -- Value is any valid attr-list value `:help attr-list`
-            functions = "NONE",
+            functions = "italic",
             keywords = "bold",
             variables = "NONE",
             conditionals = "NONE",
-            constants = "NONE",
+            constants = "bold",
             numbers = "NONE",
             operators = "NONE",
             strings = "NONE",
@@ -153,15 +55,147 @@ return {
             -- ...
           },
         },
-        palettes = {},
-        specs = {},
+        palettes = {
+          github_dark = {
+            --   orange = "#808080", -- Substituted with a medium gray
+            --
+            --   black = { base = "#000000", bright = "#1a1a1a" },
+            --   gray = { base = "#808080", bright = "#808080" },
+            --   blue = { base = "#666666", bright = "#4d4d4d" },
+            --   green = { base = "#666666", bright = "#4d4d4d" },
+            --   magenta = { base = "#666666", bright = "#4d4d4d" },
+            --   pink = { base = "#666666", bright = "#4d4d4d" },
+            --   red = { base = "#666666", bright = "#4d4d4d" },
+            --   white = { base = "#e6e6e6", bright = "#e6e6e6" },
+            --   yellow = { base = "#666666", bright = "#4d4d4d" },
+            --   cyan = { base = "#999999", bright = "#b3b3b3" },
+            --
+            --   fg = {
+            --     default = "#e6e6e6",
+            --     muted = "#b3b3b3",
+            --     subtle = "#808080",
+            --     on_emphasis = "#ffffff",
+            --   },
+            --
+            --   canvas = {
+            --     default = "#b3b3b3",
+            --     overlay = "#e6e6e6",
+            --     inset = "#cccccc",
+            --     subtle = "#e6e6e6",
+            --   },
+            --
+            --   border = {
+            --     default = "#e6e6e6",
+            --     muted = "#cccccc",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   neutral = {
+            --     emphasis_plus = "#808080",
+            --     emphasis = "#808080",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   accent = {
+            --     fg = "#999999",
+            --     emphasis = "#808080",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   success = {
+            --     fg = "#808080",
+            --     emphasis = "#999999",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   attention = {
+            --     fg = "#808080",
+            --     emphasis = "#999999",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   severe = {
+            --     fg = "#808080",
+            --     emphasis = "#999999",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   danger = {
+            --     fg = "#808080",
+            --     emphasis = "#999999",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   open = {
+            --     fg = "#808080",
+            --     emphasis = "#999999",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   done = {
+            --     fg = "#808080",
+            --     emphasis = "#999999",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   closed = {
+            --     fg = "#808080",
+            --     emphasis = "#999999",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+            --
+            --   sponsors = {
+            --     fg = "#808080",
+            --     emphasis = "#999999",
+            --     muted = "#999999",
+            --     subtle = "#cccccc",
+            --   },
+          },
+        },
+        specs = {
+          github_dark = {
+            syntax = {
+              bracket = "#f2cc60",
+              -- builtin0 = "#808080",
+              -- builtin1 = "#666666",
+              -- builtin2 = "#808080",
+              -- comment = "#4d4d4d",
+              -- conditional = "#666666",
+              -- const = "#808080",
+              -- dep = "#666666",
+              field = "#f0f6fc",
+              func = "#7ee787",
+              -- ident = "#ffffff",
+              keyword = "#f0883e",
+              -- number = "#808080",
+              operator = "#808080",
+              -- param = "#e6e6e6",
+              -- preproc = "#666666",
+              -- regex = "#4d4d4d",
+              -- statement = "#666666",
+              string = "#cae8ff",
+              -- type = "#b3b3b3",
+              -- tag = "#e6e6e6",
+              -- variable = "#e6e6e6",
+            },
+          },
+        },
         groups = {},
       })
 
       -- vim.cmd("colorscheme github_dark")
       -- vim.cmd("colorscheme github_dark_colorblind")
-      vim.cmd("colorscheme github_dark_tritanopia")
-      -- vim.cmd("colorscheme github_dark_dimmed")
+      -- vim.cmd("colorscheme github_dark_tritanopia")
+      vim.cmd("colorscheme github_dark_dimmed")
     end,
   },
 }
