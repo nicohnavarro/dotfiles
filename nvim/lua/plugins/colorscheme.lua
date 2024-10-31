9ac11e32bb3395fb40ebd1273154ab53449781c4
Nicolás Navarro <nicohnavarro@gmail.com>
2024-10-31 09:29:20 +0100

    Update theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -25,7 +25,7 @@ return {
           hide_nc_statusline = true, -- Override the underline style for non-active statuslines
           transparent = false, -- Disable setting bg (make neovim's background transparent)
           terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-          dim_inactive = false, -- Non focused panes set to alternative background
+          dim_inactive = true, -- Non focused panes set to alternative background
           module_default = true, -- Default enable value for modules
           styles = { -- Style to be applied to different syntax groups
             comments = "italic", -- Value is any valid attr-list value `:help attr-list`
@@ -194,8 +194,8 @@ return {
 
       -- vim.cmd("colorscheme github_dark")
       -- vim.cmd("colorscheme github_dark_colorblind")
-      vim.cmd("colorscheme github_dark_tritanopia")
-      -- vim.cmd("colorscheme github_dark_dimmed")
+      -- vim.cmd("colorscheme github_dark_tritanopia")
+      vim.cmd("colorscheme github_dark_dimmed")
     end,
   },
 }
